73c4310f42c9860fdc239021542a645ae1a06daa
yi-ge-dian <1085266008@qq.com>
2023-07-04 12:43:17 +0800

    ✨ feat: finish server abstract

--- server.go
+++ server.go
@@ -1,4 +1,4 @@
-package main
+package http_framework
 
 import (
 	"context"
@@ -11,7 +11,11 @@ import (
 	"time"
 )
 
+// HTTPOption 配置模式
 type HTTPOption func(h *HTTPServer)
+
+// HandleFunc 视图函数的签名
+type HandleFunc func(w http.ResponseWriter, r *http.Request)
 
 type Server interface {
 	// Handler 硬性要求，必须要组合 http.Handler
@@ -20,6 +24,8 @@ type Server interface {
 	Start(address string) error
 	// Stop	关闭服务
 	Stop() error
+	// add Router 注册路由，核心 API
+	addRouter(method string, pattern string, handlefunc HandleFunc)
 }
 
 type HTTPServer struct {
@@ -27,6 +33,8 @@ type HTTPServer struct {
 	srv *http.Server
 	// 优雅关闭的属性，通过 Option 设计模式来确定
 	stop func() error
+	// routers，临时存放路由的位置
+	routers map[string]HandleFunc
 }
 
 func WithHTTPServerStop(fn func() error) HTTPOption {
@@ -60,19 +68,30 @@ func WithHTTPServerStop(fn func() error) HTTPOption {
 }
 
 func NewHTTP(opts ...HTTPOption) *HTTPServer {
-	h := &HTTPServer{}
+	h := &HTTPServer{
+		routers: make(map[string]HandleFunc, 0),
+	}
 	for _, opt := range opts {
 		opt(h)
 	}
 	return h
 }
 
-// ServeHTTP 接受请求，转发请求、
+// ServeHTTP 接受请求，转发请求
 // 接收请求: 接收前端传过来的请求
 // 转发请求: 转发前端过来的请求到自定义的框架中
-func (h *HTTPServer) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
-	//TODO implement me
-	panic("implement me")
+// ServeHTTP 方法向前对接前端请求，向后对接咱们的框架
+func (h *HTTPServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// 1. 匹配路由
+	key := fmt.Sprintf("%s-%s", r.Method, r.URL.Path)
+	handlerFunc, ok := h.routers[key]
+	if !ok {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("404 NOT FOUND"))
+		return
+	}
+	// 2. 转发路由
+	handlerFunc(w, r)
 }
 
 // Start 开始服务
@@ -89,16 +108,37 @@ func (h *HTTPServer) Stop() error {
 	return h.stop()
 }
 
-func main() {
-	h := NewHTTP(WithHTTPServerStop(nil))
-	go func() {
-		err := h.Start(":8080")
-		if err != nil && err != http.ErrServerClosed {
-			panic("启动失败")
-		}
-	}()
-	err := h.Stop()
-	if err != nil {
-		panic("关闭失败")
-	}
+// addRouter 注册路由
+// 注册路由的时机:就是项目启动的时候注册，项目启动之后就不不能注册了
+func (h *HTTPServer) addRouter(method string, pattern string, handlefunc HandleFunc) {
+	// 构建唯一的key
+	key := fmt.Sprintf("%s-%s", method, pattern)
+	fmt.Printf("add router %s - %s\n", method, pattern)
+	h.routers[key] = handlefunc
 }
+
+func (h *HTTPServer) GET(pattern string, handleFunc HandleFunc) {
+	h.addRouter(http.MethodGet, pattern, handleFunc)
+}
+
+func (h *HTTPServer) POST(pattern string, handleFunc HandleFunc) {
+	h.addRouter(http.MethodPost, pattern, handleFunc)
+}
+
+func (h *HTTPServer) DElETE(pattern string, handleFunc HandleFunc) {
+	h.addRouter(http.MethodDelete, pattern, handleFunc)
+}
+
+// func main() {
+// 	h := NewHTTP(WithHTTPServerStop(nil))
+// 	go func() {
+// 		err := h.Start(":8080")
+// 		if err != nil && err != http.ErrServerClosed {
+// 			panic("启动失败")
+// 		}
+// 	}()
+// 	err := h.Stop()
+// 	if err != nil {
+// 		panic("关闭失败")
+// 	}
+// }
--- server_test.go
+++ server_test.go
@@ -1,9 +1,23 @@
-package main
+package http_framework
 
-import "testing"
+import (
+	"net/http"
+	"testing"
+)
 
-func TestHTTP_Starts(t *testing.T) {
+// 注册路由是在启动服务之前完成
+func Login(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Login 请求成功!"))
+}
+func Register(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Login 请求成功!"))
+}
+
+func TestHTTP_Start(t *testing.T) {
 	h := NewHTTP()
+	h.GET("/login", Login)
+	h.POST("/register", Register)
+
 	err := h.Start(":8080")
 	if err != nil {
 		t.Fail()
